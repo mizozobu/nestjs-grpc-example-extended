6791085f1e40de3e02072d5e7ec23e7a558a27a3
mizozobu <hamc.m.so.j12@gmail.com>
2020-12-06 00:24:18 +0900

    remove http app

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,10 @@ import { AppModule } from './app.module';
 import { grpcClientOptions } from './grpc-client.options';
 
 async function bootstrap() {
-  /**
-   * This example contains a hybrid application (HTTP + gRPC)
-   * You can switch to a microservice with NestFactory.createMicroservice() as follows:
-   *
-   * const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-   *  transport: Transport.GRPC,
-   *  options: {
-   *    package: 'hero',
-   *    protoPath: join(__dirname, './hero/hero.proto'),
-   *  }
-   * });
-   * await app.listenAsync();
-   *
-   */
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, grpcClientOptions);
 
-  await app.startAllMicroservicesAsync();
-  await app.listen(3001);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listenAsync();
+  console.log(`Application is running on: ${grpcClientOptions.options.url || 'localhost:5000'}`);
 }
+
 bootstrap();
